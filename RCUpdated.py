fef130bea1f41f1c11db1373088093b874669566
samfinard <104854051+samfinard@users.noreply.github.com>
2023-12-12 19:39:58 -0500

    Add files via upload

--- RCUpdated.py
+++ RCUpdated.py
@@ -1,141 +1,112 @@
 import cv2
 import numpy as np
-import time
 
 class ObjectDetector:
     def __init__(self):
-        # Color detection thresholds
-        self.blue_lower = np.array([110, 50, 50])    
-        self.blue_upper = np.array([130, 255, 255])  
-        self.yellow_lower = np.array([20, 100, 100])      
-        self.yellow_upper = np.array([30, 255, 255])  
+        # HSV color range for blue
+        self.blue_lower = np.array([110, 50, 50])
+        self.blue_upper = np.array([130, 255, 255])
 
-        # Minimum contour area to be considered as a valid object
-        self.min_contour_area = 2500
+        # HSV color range for yellow
+        self.yellow_lower = np.array([20, 100, 100])
+        self.yellow_upper = np.array([30, 255, 255])
 
-    def detect_blue(self, frame):
+        # Minimum contour area for detection
+        self.min_contour_area = 500
+
+    def detect_color(self, frame, color_lower, color_upper):
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        blue_mask = cv2.inRange(hsv, self.blue_lower, self.blue_upper)
-        return blue_mask
+        mask = cv2.inRange(hsv, color_lower, color_upper)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        return contours
 
-    def detect_green(self, frame):
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        green_mask = cv2.inRange(hsv, self.green_lower, self.green_upper)
-        return green_mask
+    def detect_objects(self, frame):
+        contours_blue = self.detect_color(frame, self.blue_lower, self.blue_upper)
+        contours_yellow = self.detect_color(frame, self.yellow_lower, self.yellow_upper)
+        return contours_blue, contours_yellow
 
-    def detect_object(self, frame):
-        blue_mask = self.detect_blue(frame)
-        green_mask = self.detect_green(frame)
-
-        contours_blue, _ = cv2.findContours(blue_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours_green, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        return contours_blue, contours_green
-
-class RCCarGame:
-    def __init__(self, left_threshold=0.3, right_threshold=0.3, gold_collection_interval=3):
-        self.left_threshold = left_threshold
-        self.right_threshold = right_threshold
-        self.gold_collection_interval = gold_collection_interval
+class GameController:
+    def __init__(self, frame_width, left_zone_ratio=0.3, right_zone_ratio=0.7):
+        self.left_zone = int(frame_width * left_zone_ratio)
+        self.right_zone = int(frame_width * right_zone_ratio)
         self.score = 0
-        self.gold_collected = 0
-        self.last_gold_collection_time = None
+        self.target_left = True
 
-    def determine_position(self, x, width):
-        if x < width * self.left_threshold:
-            return "left"
-        elif x > width * (1 - self.right_threshold):
-            return "right"
-        else:
-            return "middle"
+    def update_score_and_zone(self, object_x):
+        if (self.target_left and object_x < self.left_zone) or \
+           (not self.target_left and object_x > self.right_zone):
+            self.score += 1
+            self.target_left = not self.target_left
 
-    def update_game_state(self, position, x, frame_width):
-        current_time = time.time()
+    def get_score(self):
+        return self.score
 
-        if position == "left":
-            if self.last_gold_collection_time is None:
-                self.last_gold_collection_time = current_time
-            elif current_time - self.last_gold_collection_time >= self.gold_collection_interval:
-                self.gold_collected += 1
-                self.last_gold_collection_time = current_time
-                print(f"Gold piece collected! Total Gold: {self.gold_collected}")
+    def is_target_left(self):
+        return self.target_left
 
-        elif position == "right":
-            self.score += self.gold_collected
-            if self.gold_collected > 0:
-                print(f"Gold deposited! Score: {self.score}")
-            self.gold_collected = 0
+class VideoProcessor:
+    def __init__(self, detector, game_controller, original_frame_width):
+        self.detector = detector
+        self.game_controller = game_controller
+        self.cap = cv2.VideoCapture(0)
+        self.crop_coords = ((400, 160), (1100, 1600))
+        self.original_frame_width = original_frame_width
 
-        # Reset the gold collection timer if the car leaves the left zone
-        elif not position == "left" and self.last_gold_collection_time is not None:
-            self.last_gold_collection_time = None
-
-def video_stream_loop(cap, object_detector, game):
-    while True:
-        ret, frame = cap.read()
+    def process_frame(self):
+        ret, frame = self.cap.read()
         if not ret:
-            break
+            return False
         
-        # Crop the carpet 
-        frame_height = frame.shape[0]
-        frame_width = frame.shape[1]
-        # X1, Y1 = 90, 650 # Bottom left corner
-        # X2, Y2, = 1260, 325 # Top right corner
-        # frame = frame[Y1:Y2, X1:X2]
-
-        contours_blue, contours_green = object_detector.detect_object(frame)
-
-        # Process Blue Contours (RC Car)
+        frame = frame[self.crop_coords[0][0]:self.crop_coords[1][0], self.crop_coords[0][1]:self.crop_coords[1][1]]
+        contours_blue, contours_yellow = self.detector.detect_objects(frame)
         for contour in contours_blue:
-            if cv2.contourArea(contour) < object_detector.min_contour_area:
-                continue
-            (x, y, w, h) = cv2.boundingRect(contour)
-            center_x = (x + w) // 2
-            center_y = (y + h) // 2
-            position = game.determine_position(center_x, frame_width)
-            game.update_game_state(position, center_x, frame_width)
-            print(f"Rover Coordinates: X={center_x}, Y={center_y}")  # Print the coordinates of the rover
-            cv2.circle(frame, (center_x, center_y), 15, (255, 0, 0), 2)  # Green circle for RC Car
+            if cv2.contourArea(contour) > self.detector.min_contour_area:
+                x, y, w, h = cv2.boundingRect(contour)
+                centroid_x = x + w // 2
+                self.game_controller.update_score_and_zone(centroid_x)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-        # Process Green Contours (Rover)
-        for contour in contours_green:
-            if cv2.contourArea(contour) < object_detector.min_contour_area:
-                continue
-            (x, y, w, h) = cv2.boundingRect(contour)
-            center_x = (x + w) // 2
-            center_y = (y + h) // 2
-            cv2.circle(frame, (center_x, center_y), 15, (0, 255, 0), 2) 
+        self.draw_contours(frame, contours_yellow, (255, 255, 0), self.detector.min_contour_area)
+        self.display_frame(frame)
+        return True
 
-        # Draw threshold lines
-        frame_width = frame.shape[1]
-        left_threshold_x = int(frame_width * game.left_threshold)
-        right_threshold_x = int(frame_width * (1 - game.right_threshold))
-        cv2.line(frame, (left_threshold_x, 0), (left_threshold_x, frame.shape[0]), (255, 0, 0), 2)
-        cv2.line(frame, (right_threshold_x, 0), (right_threshold_x, frame.shape[0]), (255, 0, 0), 2)
+    def draw_contours(self, frame, contours, color, min_contour_area):
+        for contour in contours:
+            if cv2.contourArea(contour) > min_contour_area:
+                (x, y, w, h) = cv2.boundingRect(contour)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
 
-        # Display the processed frame
-        cv2.imshow("Video Stream", frame)
+    def display_frame(self, frame):
+        frame_height, frame_width, _ = frame.shape
 
-        # Exit the loop when 'q' is pressed
+        # Update coordinates based on the cropped frame size
+        left_zone = int(self.game_controller.left_zone * frame_width / self.original_frame_width)
+        right_zone = int(self.game_controller.right_zone * frame_width / self.original_frame_width)
+
+        # Draw lines at left and right zones
+        cv2.line(frame, (left_zone, 0), (left_zone, frame_height), (0, 255, 0), 2)
+        cv2.line(frame, (right_zone, 0), (right_zone, frame_height), (0, 255, 0), 2)
+        cv2.putText(frame, f'Score: {self.game_controller.get_score()}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        cv2.imshow('Object Detector', frame)
+
+    def release_resources(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+def main():
+    detector = ObjectDetector()
+    cap = cv2.VideoCapture(0)  # Capture video
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # Get original frame width
+    processor = VideoProcessor(detector, GameController(frame_width=frame_width), original_frame_width=frame_width)
+
+    while True:
+        if not processor.process_frame():
+            break
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release the camera and close all windows
-    cap.release()
-    cv2.destroyAllWindows()
-
-def main():
-    cap = cv2.VideoCapture(0)
-    object_detector, game = ObjectDetector(), RCCarGame()
-
-    ret, frame = cap.read()
-    if not ret:
-        print("Failed to capture initial frame")
-        cap.release()
-        return
-    
-    video_stream_loop(cap, object_detector, game)
+    processor.release_resources()
 
 if __name__ == "__main__":
     main()
-    
